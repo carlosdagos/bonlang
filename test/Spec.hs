fc5b0ca0d789972c40364bb78e126153299aac4b
Carlos D'Agostino <carlos.dagostino@gmail.com>
2016-07-25 18:00:12 +0200

    add add examples to parse tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,23 +11,13 @@ main :: IO ()
 main = do tests' <- tests
           defaultMain tests'
 
-testFiles :: IO [String]
-testFiles = do let testDir = "test/parser_tests/"
-               files <- getDirectoryContents testDir
-               let filtered = filter (\f -> takeExtension f == ".bl") files
-               return $ map (testDir ++) filtered
-
-parse' :: String -> IO (String, Bool)
-parse' f = parseFromFile bonlangParser f >>= \r -> return (f, parsedCorr r)
-    where
-        parsedCorr (Left _)  = False
-        parsedCorr (Right _) = True
-
 tests :: IO TestTree
-tests = parsing >>= \p -> return $ testGroup "Bonlang" [p]
+tests = parsing
+    >>= \p -> return $ testGroup "Bonlang" [p]
 
 parsing :: IO TestTree
-parsing = testFiles >>= \files -> return $ testGroup "Parsing tests" $ map testCaseFiles files
+parsing = testFiles
+      >>= \files -> return $ testGroup "Parsing tests" $ map testCaseFiles files
 
 testCaseFiles :: String -> TestTree
 testCaseFiles f = testCase ("Parsing file: " `mappend` f) fileAssertion
@@ -35,4 +25,18 @@ testCaseFiles f = testCase ("Parsing file: " `mappend` f) fileAssertion
         fileAssertion :: Assertion
         fileAssertion = do (_, b) <- parse' f
                            b @?= True
+
+testFiles :: IO [String]
+testFiles = do let testDir = "test/parser_tests/"
+               files <- getDirectoryContents testDir
+               let filtered = filter (\f -> takeExtension f == ".bl") files
+               return $ map (testDir ++) filtered
+
+parse' :: String -> IO (String, Bool)
+parse' f = parseFromFile bonlangParser f
+       >>= \r -> return (f, parsedCorr r)
+    where
+        parsedCorr (Left _)  = False
+        parsedCorr (Right _) = True
+
 
